6b49651a200b5667b73e55f0291d50f7b7f65c63
Poper <popernennins@national.shitposting.agency>
2019-03-05 19:41:06 -0600

    Added loader

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var loader = document.getElementById("loader");
+loader.style.display = "none";
+
 function doesntWork(){
   alert("Doesn't work yet");
+}
+
+function play(){
+  var fpelements = document.getElementById("frontpage");
+  fpelements.parentNode.removeChild(fpelements);
+  loader.style.display = "flex";
 }
